cb8b0c5327121290fac6c4af78ae9310f56638fc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-24 01:48:35 -0300

    removendo_arquivo_duplicado

--- concatenacao/src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-
-    val nome = "Luis Henrique Silva Ferreira"
-    val graduacao = "Análise e desenvolvimento de sistemas"
-    val tecnico1 = "Informática para internet"
-    val tecnico2 = "Eletroeletrônica"
-
-    println("$ chamo $nome sou formando em '$graduacao' e possuo cursos técnico de '$tecnico1' e '$tecnico2'")
-}
