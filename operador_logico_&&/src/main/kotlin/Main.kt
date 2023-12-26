e0967b7005cb3f760db4b80239140158943c7600
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-26 00:34:10 -0300

    removendo_arquivo_duplicado

--- operador_logico_&&/src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>) {
-
-    if(20 == 20 && 50 > 20 && 10 < 20) {        // operador lógico '&&' equivala a 'e', se os dois lados não forem 'verdadeiros' o retorno será 'falso'
-        println("verdadeiro")
-    } else {
-        println("falso")
-    }
-}
