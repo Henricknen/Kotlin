df134152997af84c523fa79337650bdde2e17fe0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-28 00:30:52 -0300

    criando_exemplos_de_'operadores_relacionais'_de_'igualdade'_'menor'_'maior'_'menor_ou_igual'_'maior_ou_igual'_e_'diferença_entre_dois_valores'

--- operadores_relacionais.kt
+++ operadores_relacionais.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     var valor1 = 20
     var valor2 = 60
 
-    println(valor1 == valor2)       // 'igualdade'
+    println(valor1 == valor2)       // 'igualdade' 
     println("---")
     println(valor1 < valor2)    // 'menor'
     println("---")
